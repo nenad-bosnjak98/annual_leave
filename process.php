7509b9e29df7fed369b75d47bd09a8700503b702
nenad-bosnjak98 <nenad-bosnjak98>
2021-06-19 15:54:47 +0200

    Added if condition to check if the user input and the JSON file contents are the same, deleted some unnecessary code

--- process.php
+++ process.php
@@ -1,14 +1,18 @@
 <?php
 
-if(isset($_POST['fullname']) && isset($_POST['designation']) && isset($_POST['date']) && (isset($_POST['fulltime']) || isset($_POST['parttime']))) {
+$employees = file_get_contents('js/employees.json');
+$emparray = json_decode($employees, true);
+
+
+
+
+if(isset($_POST['fullname']) && isset($_POST['designation']) && isset($_POST['date'])) {
     if(isset($_POST['fulltime'])) {
-        echo $_POST['fullname'];
-        echo "<br>";
-        echo $_POST['designation'];
-        echo "<br>";
-        echo $_POST['date'];
-        echo "<br>";
-        echo $_POST['fulltime'];
+        foreach($emparray as $emp) {
+            if($emp['full_name'] == $_POST['fullname'] && $emp['designation'] == $_POST['designation'] && $emp['start_date'] == $_POST['date'] ) {
+                print_r($emp);
+            }
+        }
     }
     if(isset($_POST['parttime'])) {
         print $_POST['fullname'];
