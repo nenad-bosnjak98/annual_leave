522d93107f6c70d0966e9dcc00fe31b4ef787b86
nenad-bosnjak98 <nenad-bosnjak98>
2021-06-20 16:57:54 +0200

    Added an if statement of the formula and also calculated the months from last year and this year to keep track of annual leave days per year.

--- process.php
+++ process.php
@@ -3,7 +3,7 @@
 $employees = file_get_contents('js/employees.json');
 $emparray = json_decode($employees, true);
 $endOfYear = new DateTime('last day of December last year');
-$beginningOfYear = new DateTime('first day of this year');
+$beginningOfYear = new DateTime('first day of January this year');
 
 $date = new DateTime('now');
 $june = date('Y-06-30');
@@ -18,6 +18,39 @@ if(isset($_POST['fullname']) && isset($_POST['designation']) && isset($_POST['sd
                     $dateobj = new DateTime($emp['start_date']);
                     $realdate = $date->diff($dateobj);
                     $days = $realdate->days;
+
+                    
+                    // Calculating ammount of months last year and this year
+
+                    $dateobj1 = new DateTime($_POST['sdate']);
+                    $lastyear = $dateobj1->diff($endOfYear);
+                    $monthslastyear = round($lastyear->y*12 + $lastyear->m + $lastyear->d / 30);
+
+                    echo $monthslastyear;
+                    echo '<br>';
+
+                    $thisyear = $beginningOfYear->diff($date);
+                    $monthsthisyear = round($thisyear->y*12 + $thisyear->m + $thisyear->d / 30);
+
+                    echo $monthsthisyear;
+                    echo '<br>';
+
+                    // Remaining days last year
+                    $dayslastyear = 20/12 * $monthslastyear;
+
+
+
+
+                    // Conditions for calculating the annual leave allowed days
+                    if($days > 365) {
+                        $formula = 20 + $dayswithoutweekends;
+                    }
+                    else {
+                        
+                        $formula = round(20/12 * $months);
+                    }
+
+                    echo $formula;
 
 
                 }
