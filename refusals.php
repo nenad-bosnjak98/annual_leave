d29dec05b46eb58a6cc4a1b81832abd4b2bd5bc0
nenad-bosnjak98 <nenad-bosnjak98>
2021-06-21 22:04:24 +0200

    Project completed

--- refusals.php
+++ refusals.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/admin.css">
-    <title>Approved Requests</title>
+    <title>Refused Requests</title>
 </head>
 <body>
 <div class='jsontxt' id="files">
